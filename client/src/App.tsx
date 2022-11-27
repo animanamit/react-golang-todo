04b382238548eb3a4385092aa77ca7f91c3b6d64
Animan Amit <animanamit@animans-air.lan>
2022-11-27 23:31:24 +0800

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen mx-auto w-full flex items-center justify-center">
+      <h1 className="text-2xl text-red-400">hi</h1>
+    </div>
+  );
+}
+
+export default App;
